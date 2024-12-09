c805579521e9097c7b783561ddd7fa9db822c60c
tuandbing <1873656423@qq.com>
2024-12-09 16:20:09 +0800

    启用禁用员工账号业务

--- sky-server/src/main/java/com/sky/controller/admin/EmployeeController.java
+++ sky-server/src/main/java/com/sky/controller/admin/EmployeeController.java
@@ -97,4 +97,14 @@ public class EmployeeController {
         return Result.success(pageResult);
     }
 
+    @ApiOperation(value = "启用禁用员工账号")
+    @PostMapping("/status/{status}")
+    public Result startOrStop(@PathVariable Integer status, Long id) {
+        log.info("启用禁用员工 {} ：{}", status, id);
+
+        employeeService.startOrStop(status, id);
+
+        return Result.success();
+    }
+
 }
